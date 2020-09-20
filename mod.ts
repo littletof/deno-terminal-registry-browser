0a12079cc36dc6fc377cf899f3cb2446e4c3e5ff
littletof <szalay.kristof.sopron@gmail.com>
2020-09-20 21:08:51 +0200

    basics

---
+++ mod.ts
@@ -0,0 +1,139 @@
+import { Select } from "https://deno.land/x/cliffy@v0.14.1/prompt/select.ts";
+import { Input } from "https://deno.land/x/cliffy/prompt/input.ts";
+import  * as colors from 'https://deno.land/std@0.70.0/fmt/colors.ts';
+
+interface RegistryDef {
+    name: string;
+    init: () => Promise<void> | void | undefined;
+    getModulesPage: (page: number, pageSize: number, query?: string) => RegistryListItem[] | Promise<RegistryListItem[]>;
+    showInfoPage: (module: string) => void | Promise<void>;
+}
+
+interface RegistryListItem {
+    name: string;
+    value: string;
+}
+
+const up1Line = "\x1B[1A";
+const workingMem: any = {};
+const stateMachine: {[key: string]: (selectedMenu: string) => void | Promise<void>} = {
+    'select_registry': async (selectedMenu: string) => {
+        workingMem.registry = selectedMenu;
+        workingMem.page = 1;
+        workingMem.pageSize = 10;
+        state = 'registry_home';
+        await registries[selectedMenu].init();
+    },
+    'registry_home': async (selectedMenu: string) => {
+        switch (selectedMenu) {
+            case 'next_page_x': workingMem.page++; break;
+            case 'prev_page_x': workingMem.page--; break;
+            case 'back_x': state = 'select_registry'; break;
+            case 'search_module_x': state = 'search_module'; break;
+            default: state ='module_info'; workingMem.selectedModule = selectedMenu; break;
+        }
+    },
+    'search_module': async (selectedMenu: string) => {
+        switch (selectedMenu) {
+            case 'back_x': state = 'registry_home'; break;
+            default: state ='module_info'; workingMem.selectedModule = selectedMenu; break;
+        }
+    }
+};
+let state = 'select_registry';
+
+async function getMenu() {
+    switch(state) {
+        case 'select_registry':
+            return {title: 'Select registry', options: Object.keys(registries).map(r => ({name: r, value: r}))};
+        case 'registry_home':
+            const rMenu = await registries[workingMem.registry].getModulesPage(workingMem.page, workingMem.pageSize);
+            const totalPages = Math.ceil(workingMem.totalModules / workingMem.pageSize);
+            return {title: workingMem.registry, options: [
+                { name: "", value: "", disabled: true},
+                ...rMenu,
+                { name: "", value: "", disabled: true},
+                Select.separator(`---- ${workingMem.page} / ${totalPages} (${workingMem.totalModules} modules)----`),
+                { name: menuColor("Next page", workingMem.page === totalPages), value: "next_page_x", disabled: workingMem.page === totalPages },
+                { name: menuColor("Previous page", workingMem.page === 1), value: "prev_page_x", disabled: workingMem.page === 1 },
+                { name: "Search Module", value: "search_module_x"},
+                { name: "Back", value: "back_x"},
+            ], default: workingMem.page !== totalPages? menuColor("Next page", workingMem.page === totalPages): menuColor("Previous page", workingMem.page === 1)};
+        case 'search_module':
+            workingMem.query = await Input.prompt(`${'\u0008'.repeat(5)}Searching for module: `);
+            workingMem.page = 1;
+            const qMenu = await registries[workingMem.registry].getModulesPage(workingMem.page, workingMem.pageSize, workingMem.query);
+            return {
+                title: `Searching in ${workingMem.registry} for: "${workingMem.query}"`, options: [
+                    { name: "", value: "", disabled: true},
+                    ...qMenu,
+                    { name: "", value: "", disabled: true},
+                    Select.separator(`---- Searching for: "${workingMem.query}" ----`),
+                    { name: "Back", value: "back_x"},
+                ]
+            };
+        case 'module_info':
+            await registries[workingMem.registry].showInfoPage(workingMem.selectedModule);
+            return {title: `${'\u0008'.repeat(5)}Action:`, options: []};
+    }
+}
+/* { name: "Next page", value: "next_page_x" },
+        { name: "Previous page", value: "prev_page_x"}, */
+
+const registries: {[key: string]: RegistryDef} = {
+    "deno.land": {
+        name: "deno.land/x",
+        init: () => {},
+        getModulesPage: async (page: number, pageSize: number, query?: string) => {
+            const response = await ((await fetch(`https://api.deno.land/modules?page=${page}&limit=${pageSize}${query? `&query=${query}`: ""}`)).json());
+            workingMem.totalModules = response.data.total_count;
+            return (response.data.results  as any[]).map(m => ({name: `${`${colors.white(m.star_count.toString())}${colors.yellow("*")}`.padStart(26)} ${colors.green(m.name.padEnd(15))} - ${(m.description as string)?.slice(0, 50)}`, value: m.name}));
+        },
+        showInfoPage: (module: string) => { console.log('\n' + module);}
+    },
+    "nest.land": {
+        name: "x.next.land",
+        init: async () => {
+            const response = await ((await fetch(`https://x.nest.land/api/packages`)).json());
+            workingMem.nestModules = response;
+            workingMem.totalModules = response.length;
+        },
+        getModulesPage: (page: number, pageSize: number, query?: string) => {
+            const filteredModules = query ? (workingMem.nestModules as any[]).filter(m => (m.name as string).indexOf(query) !== -1 || m.description.indexOf(query) !== -1) : (workingMem.nestModules as any[]);
+            const modulesOnPage = (filteredModules as any[]).slice((page-1) * pageSize, Math.min(filteredModules.length, page * pageSize));
+            return modulesOnPage.map(m => ({name: `${colors.green(m.name.padEnd(15))} - ${(m.description as string)?.slice(0, 50)}`, value: m.name}));
+        },
+        showInfoPage: (module: string) => { console.log('\n' + module);}
+    }
+}
+
+let selectedMenu;
+while(selectedMenu !== 'exit') {
+  selectedMenu = await showModuleMenu();
+  console.log(`\r${up1Line}${up1Line}`);
+  await stateMachine[state](selectedMenu);
+}
+
+async function showModuleMenu() {
+
+    const currentMenu = await getMenu();
+
+    return await Select.prompt({
+      message: `${'\u0008'.repeat(5)}${currentMenu?.title}`,
+      options: currentMenu!.options,
+      keys: {
+          next: ['s'],
+          previous: ['w']
+      },
+      default: currentMenu!.default,
+      maxRows: 17,
+    });
+  }
+
+  function menuColor(name: string, disabled: boolean) {
+    if(!disabled) {
+        return colors.bold(colors.white(name));
+    } else {
+        return colors.gray(name);
+    }
+  }
